75d4aa2674ac4165d8bc526e688617c25c942e6e
David Lannan <dlannan68@gmail.com>
2024-09-11 22:37:21 +0930

    load skeleton and animation working.

--- ozz-animation/src/ozz-animation.cpp
+++ ozz-animation/src/ozz-animation.cpp
@@ -1,40 +1,155 @@
 // ozz.cpp
 // Extension lib defines
-#define LIB_NAME "ozz"
-#define MODULE_NAME "ozz"
+#define LIB_NAME "ozzanim"
+#define MODULE_NAME "ozzanim"
 
 // include the Defold SDK
 #include <dmsdk/sdk.h>
+#include <string>
+#include <vector>
 
-static int Reverse(lua_State* L)
+#include "ozz/animation/runtime/animation.h"
+#include "ozz/animation/runtime/local_to_model_job.h"
+#include "ozz/animation/runtime/sampling_job.h"
+#include "ozz/animation/runtime/skeleton.h"
+#include "ozz/base/io/archive.h"
+#include "ozz/base/io/stream.h"
+#include "ozz/base/log.h"
+#include "ozz/base/containers/vector.h"
+#include "ozz/base/maths/simd_math.h"
+#include "ozz/base/maths/soa_transform.h"
+#include "ozz/base/maths/vec_float.h"
+#include "ozz/options/options.h"
+
+typedef struct animObj
 {
-    // The number of expected items to be on the Lua stack
-    // once this struct goes out of scope
+    std::string     skeleton_filename;
+    std::string     animation_filename;
+
+    ozz::animation::Skeleton skeleton;
+    ozz::animation::Animation animations;
+    ozz::animation::SamplingJob::Context context;
+    ozz::vector<ozz::math::SoaTransform> locals;
+    ozz::vector<ozz::math::Float4x4> models;    
+} _animObj;
+    
+static std::vector<animObj *> g_anims;
+
+bool LoadSkeleton(const char* _filename, ozz::animation::Skeleton* _skeleton) {
+  assert(_filename && _skeleton);
+  ozz::log::Out() << "Loading skeleton archive " << _filename << "."
+                  << std::endl;
+  ozz::io::File file(_filename, "rb");
+  if (!file.opened()) {
+    ozz::log::Err() << "Failed to open skeleton file " << _filename << "."
+                    << std::endl;
+    return false;
+  }
+  ozz::io::IArchive archive(&file);
+  if (!archive.TestTag<ozz::animation::Skeleton>()) {
+    ozz::log::Err() << "Failed to load skeleton instance from file "
+                    << _filename << "." << std::endl;
+    return false;
+  }
+
+  // Once the tag is validated, reading cannot fail.
+  {
+    archive >> *_skeleton;
+  }
+  return true;
+}
+
+bool LoadAnimation(const char* _filename, ozz::animation::Animation* _animation) {
+  assert(_filename && _animation);
+  ozz::log::Out() << "Loading animation archive: " << _filename << "."
+                  << std::endl;
+  ozz::io::File file(_filename, "rb");
+  if (!file.opened()) {
+    ozz::log::Err() << "Failed to open animation file " << _filename << "."
+                    << std::endl;
+    return false;
+  }
+  ozz::io::IArchive archive(&file);
+  if (!archive.TestTag<ozz::animation::Animation>()) {
+    ozz::log::Err() << "Failed to load animation instance from file "
+                    << _filename << "." << std::endl;
+    return false;
+  }
+
+  // Once the tag is validated, reading cannot fail.
+  {
+    archive >> *_animation;
+  }
+
+  return true;
+}
+
+
+static int LoadOzz(lua_State* L)
+{
     DM_LUA_STACK_CHECK(L, 1);
 
-    // Check and get parameter string from stack
-    char* str = (char*)luaL_checkstring(L, 1);
-
-    // Reverse the string
-    int len = strlen(str);
-    for(int i = 0; i < len / 2; i++) {
-        const char a = str[i];
-        const char b = str[len - i - 1];
-        str[i] = b;
-        str[len - i - 1] = a;
+    const char *skeleton_filename = (char *)luaL_checkstring(L, 1);
+    if(skeleton_filename == nullptr)
+    {
+        printf("[LoadOzz Error] Invalid skeleton filename\n");
+        lua_pushnil(L);
+        return 1;    
     }
 
-    // Put the reverse string on the stack
-    lua_pushstring(L, str);
+    const char *animation_filename = (char *)luaL_checkstring(L, 2);
+    if(animation_filename == nullptr)
+    {
+        printf("[LoadOzz Error] Invalid animation filename\n");
+        lua_pushnil(L);
+        return 1;    
+    }
 
-    // Return 1 item
+    animObj *anim = new animObj();
+    anim->skeleton_filename = skeleton_filename;
+    anim->animation_filename = animation_filename;
+
+    // Reading skeleton.
+    if (!LoadSkeleton(anim->skeleton_filename.c_str(), &anim->skeleton)) {
+        printf("[LoadOzz Error] cannot load skeleton: %s.\n", anim->skeleton_filename.c_str());
+        lua_pushnil(L);
+        return 1;
+    }
+
+    // Reading animation
+    if (!LoadAnimation(anim->animation_filename.c_str(), &anim->animations)) {
+        printf("[LoadOzz Error] cannot load animation: %s.\n", anim->animation_filename.c_str());
+        lua_pushnil(L);
+        return 1;
+    }
+
+    // Allocates runtime buffers.
+    const int num_soa_joints = anim->skeleton.num_soa_joints();
+    anim->locals.resize(num_soa_joints);
+    const int num_joints = anim->skeleton.num_joints();
+    anim->models.resize(num_joints);
+
+    // Allocates a context that matches animation requirements.
+    anim->context.Resize(num_joints);
+
+    // Skeleton and animation needs to match.
+    if (anim->skeleton.num_joints() != anim->animations.num_tracks()) {
+        printf("[LoadOzz Error] joints and tracks do not match.\n");
+        lua_pushnil(L);
+        return 1;
+    }    
+
+    int idx = g_anims.size();
+    g_anims.push_back(anim);
+
+    lua_pushnumber(L, idx);
     return 1;
 }
 
 // Functions exposed to Lua
 static const luaL_reg Module_methods[] =
 {
-    {"reverse", Reverse},
+    {"loadozz", LoadOzz},
     {0, 0}
 };
 
@@ -49,13 +164,13 @@ static void LuaInit(lua_State* L)
     assert(top == lua_gettop(L));
 }
 
-static dmExtension::Result AppInitializeozz(dmExtension::AppParams* params)
+static dmExtension::Result AppInitializeozzanim(dmExtension::AppParams* params)
 {
     dmLogInfo("AppInitializeozz");
     return dmExtension::RESULT_OK;
 }
 
-static dmExtension::Result Initializeozz(dmExtension::Params* params)
+static dmExtension::Result Initializeozzanim(dmExtension::Params* params)
 {
     // Init Lua
     LuaInit(params->m_L);
@@ -63,42 +178,46 @@ static dmExtension::Result Initializeozz(dmExtension::Params* params)
     return dmExtension::RESULT_OK;
 }
 
-static dmExtension::Result AppFinalizeozz(dmExtension::AppParams* params)
+static dmExtension::Result AppFinalizeozzanim(dmExtension::AppParams* params)
 {
     dmLogInfo("AppFinalizeozz");
+    for(int i=0; i<g_anims.size(); i++)
+    {
+        delete g_anims[i];
+    }
     return dmExtension::RESULT_OK;
 }
 
-static dmExtension::Result Finalizeozz(dmExtension::Params* params)
+static dmExtension::Result Finalizeozzanim(dmExtension::Params* params)
 {
     dmLogInfo("Finalizeozz");
     return dmExtension::RESULT_OK;
 }
 
-static dmExtension::Result OnUpdateozz(dmExtension::Params* params)
+static dmExtension::Result OnUpdateozzanim(dmExtension::Params* params)
 {
     dmLogInfo("OnUpdateozz");
     return dmExtension::RESULT_OK;
 }
 
-static void OnEventozz(dmExtension::Params* params, const dmExtension::Event* event)
+static void OnEventozzanim(dmExtension::Params* params, const dmExtension::Event* event)
 {
     switch(event->m_Event)
     {
         case dmExtension::EVENT_ID_ACTIVATEAPP:
-            dmLogInfo("OnEventozz - EVENT_ID_ACTIVATEAPP");
+        dmLogInfo("OnEventozzanim - EVENT_ID_ACTIVATEAPP");
             break;
         case dmExtension::EVENT_ID_DEACTIVATEAPP:
-            dmLogInfo("OnEventozz - EVENT_ID_DEACTIVATEAPP");
+        dmLogInfo("OnEventozzanim - EVENT_ID_DEACTIVATEAPP");
             break;
         case dmExtension::EVENT_ID_ICONIFYAPP:
-            dmLogInfo("OnEventozz - EVENT_ID_ICONIFYAPP");
+        dmLogInfo("OnEventozzanim - EVENT_ID_ICONIFYAPP");
             break;
         case dmExtension::EVENT_ID_DEICONIFYAPP:
-            dmLogInfo("OnEventozz - EVENT_ID_DEICONIFYAPP");
+        dmLogInfo("OnEventozzanim - EVENT_ID_DEICONIFYAPP");
             break;
         default:
-            dmLogWarning("OnEventozz - Unknown event id");
+        dmLogWarning("OnEventozzanim - Unknown event id");
             break;
     }
 }
@@ -109,4 +228,4 @@ static void OnEventozz(dmExtension::Params* params, const dmExtension::Event* ev
 
 // ozz is the C++ symbol that holds all relevant extension data.
 // It must match the name field in the `ext.manifest`
-DM_DECLARE_EXTENSION(ozz, LIB_NAME, AppInitializeozz, AppFinalizeozz, Initializeozz, OnUpdateozz, OnEventozz, Finalizeozz)
+DM_DECLARE_EXTENSION(ozzanim, LIB_NAME, AppInitializeozzanim, AppFinalizeozzanim, Initializeozzanim, OnUpdateozzanim, OnEventozzanim, Finalizeozzanim)
